040826666245a6f61192158b03c2f34d49f843a6
Salon <rautsalon@gmail.com>
2024-03-20 18:49:33 +0545

    added username validation

--- banksystem.py
+++ banksystem.py
@@ -1,5 +1,7 @@
+import getpass
+
 class BankAccount:
-    def __init__(self,user_name : str, pass_word : str, balance : float) -> None:
+    def __init__(self, user_name: str, pass_word: str, balance: float) -> None:
         self.username = user_name
         self.password = pass_word
         self.balance = balance
@@ -10,44 +12,47 @@ class BankAccount:
     def withdraw(self, amount):
         self.balance -= amount
 
-#Task 1 Loop over N times and create N objects put it in a list
-        
-import getpass
 total_accounts = []
-n=int(input('Enter how many users to create: '))
-for i in range(n):
+
+def create_account():
     user_name = input('Enter username: ')
+    for account in total_accounts:
+        if account.username == user_name:
+            print("Username already exists. Please choose another username.")
+            return
     password = getpass.getpass('Enter password: ')
     balance = float(input('Enter bank balance: '))
-    total_accounts.append(BankAccount(user_name, password,balance))
+    total_accounts.append(BankAccount(user_name, password, balance))
+    print("Account created successfully.")
 
+# Task 1 Loop over N times and create N objects put it in a list
+n = int(input('Enter how many users to create: '))
+for _ in range(n):
+    create_account()
 
-# print(total_accounts)
-#Task 2 Take username and password from user and check which object it belong to
-#task 3 display all information of that user
-    
+# Task 2 Take username and password from user and check which object it belongs to
+# Task 3 Display all information of that user
 find_username = input('Enter the username to search: ')
 find_password = getpass.getpass('Enter password to search: ')
 found = False
-for object in total_accounts:
-    if find_username == object.username and find_password == object.password:
+for obj in total_accounts:
+    if find_username == obj.username and find_password == obj.password:
         found = True
-        print(f'Object Found') 
+        print('Object Found') 
         print('Displaying all the information of the searched object: ')
-        print(object.username)
-        print(object.password)
-        print(object.balance)
+        print(obj.username)
+        print(obj.password)
+        print(obj.balance)
         break
 if not found:
     print('Not Found')
 
-
-#Task 4 take username of person who you want to send money
-#Task 5 find the object  with that username and deposit  the required amount
-#Task 6 withdraw amount from your object  and deposit it to target object
-receiver_username = input('Enter the username of the person who you want to send money: ')
+# Task 4 Take the username of the person to whom you want to send money
+# Task 5 Find the object with that username and deposit the required amount
+# Task 6 Withdraw the amount from your object and deposit it to the target object
+receiver_username = input('Enter the username of the person you want to send money to: ')
 amount = float(input('Enter the amount to deposit: '))
-sender_username = input(f'Enter the username of the person from whom you want to send money to {receiver_username}: ')
+sender_username = input(f'Enter your username to send money to {receiver_username}: ')
 receiver_found = False
 sender_found = False
 for account in total_accounts:
